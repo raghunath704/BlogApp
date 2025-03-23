5997797bd0ce4b50b466c49181823bee5f94e789
Raghunath Das <raghunathdas704@gmail.com>
2025-03-23 21:51:17 +0530

    First Commit

---
+++ src/main/java/in/raghunath/blogapp/controller/UserController.java
@@ -0,0 +1,35 @@
+package in.raghunath.blogapp.controller;
+
+import in.raghunath.blogapp.model.User;
+import in.raghunath.blogapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // Create a new user
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    // Get all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    // Get user by username
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserByUsername(username));
+    }
+}
