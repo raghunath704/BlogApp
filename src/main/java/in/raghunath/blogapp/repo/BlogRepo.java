81f2847be5bd0481ec78f34d42ece9c6dfe7a7be
Raghunath Das <raghunathdas704@gmail.com>
2025-03-25 22:57:00 +0530

    Updated blog and user entity

--- src/main/java/in/raghunath/blogapp/repo/BlogRepo.java
+++
@@ -1,9 +0,0 @@
-package in.raghunath.blogapp.repo;
-
-import in.raghunath.blogapp.model.Blog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepo extends MongoRepository<Blog,String> {
-}
